816ad0aa30d1534daf7f26d9867f14c3b5dcfcd2
Human Friend <humanfriend22@gmail.com>
2022-09-23 20:00:40 -0700

    fix pubsub timing

--- functions/src/index.js
+++ functions/src/index.js
@@ -6,12 +6,12 @@ process.env = Object.assign(process.env, require('./config.json'));
 const instance = new Instance(process.env.INSTANCE_ID);
 
 //https://crontab.guru/#0_22_*_*_1-5
-// export const stop = functions.pubsub
-//     .schedule('0 22 * * 1-5')
-//     .timeZone('America/Los_Angeles')
-//     .onRun(async () => {
-//         functions.logger.info(await instance.stop());
-//     });
-exports.stop = functions.https.onRequest(async (req, res) => {
-    res.end(await instance.status());
-});
+exports.stop = functions.pubsub
+    .schedule('0 23 * * *')
+    .timeZone('America/Los_Angeles')
+    .onRun(async () => {
+        functions.logger.info(await instance.stop());
+    });
+// exports.stop = functions.https.onRequest(async (req, res) => {
+//     res.end(await instance.status());
+// });
