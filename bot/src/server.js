d78b5b7636061fb0d17815098f218aad443012c8
Human Friend <humanfriend22@gmail.com>
2022-10-06 16:17:09 -0700

    update bot server port

--- bot/src/server.js
+++ bot/src/server.js
@@ -2,4 +2,4 @@ const app = require('express')();
 
 app.get('/health', (req, res) => res.status(200).end(""));
 
-app.listen(3000, () => require('./bot'));
+app.listen(10000, () => require('./bot'));
