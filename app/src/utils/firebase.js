cdd39a49f90bd84752302b43a2c254c6b38243bc
humanfriend22 <humanfriend22@gmail.com>
2022-10-17 10:46:37 -0700

    fix api key permissions

--- app/src/utils/firebase.js
+++ app/src/utils/firebase.js
@@ -1,10 +1,10 @@
 import { initializeApp } from 'firebase/app';
 import { signInWithEmailAndPassword, getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
 import { getFunctions, httpsCallable } from 'firebase/functions';
 
-
 const app = initializeApp({
-    apiKey: 'AIzaSyCGn4BO1ODLIR23pmmJ0aSqsyRMu_cmnyk',
+    apiKey: import.meta.env.PROD ? 'AIzaSyCGn4BO1ODLIR23pmmJ0aSqsyRMu_cmnyk' : 'AIzaSyA82-4qHPLRVug60YtQVM-t-x48nslDTak',
     authDomain: 'cookie-jar-smp.firebaseapp.com',
     projectId: 'cookie-jar-smp',
     storageBucket: 'cookie-jar-smp.appspot.com',
@@ -13,9 +13,13 @@ const app = initializeApp({
 });
 
 const auth = getAuth(app);
+const db = getFirestore(app);
 const functions = getFunctions(app);
+let isElevatedUserDocumentReference;
 
-export const start = httpsCallable(functions, 'start');
+const sendStartCommand = httpsCallable(functions, 'start')
+
+export const start = async () => (await sendStartCommand()).data;
 export const stats = httpsCallable(functions, 'stats');
 
 export const login = async (email, password) => {
@@ -26,3 +30,9 @@ export const login = async (email, password) => {
 export const logout = () => signOut(auth);
 
 export const addAuthListener = (func) => onAuthStateChanged(auth, func);
+
+export const isElevatedUser = async () => (await getDoc(isElevatedUserDocumentReference)).exists();
+
+addAuthListener(credential => {
+    isElevatedUserDocumentReference = doc(db, '/elevated-users/' + credential.uid);
+});
